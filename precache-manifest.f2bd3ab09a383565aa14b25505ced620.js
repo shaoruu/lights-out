ba3c12af1da7a803da7858eb5cbe2f537a36ebe3
ian13456 <21ianh1@students.tas.tw>
2020-08-27 23:13:07 +0800

    Updates

---
+++ precache-manifest.f2bd3ab09a383565aa14b25505ced620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7acc23ecfe4017f7ecd60c03ab259d5e",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "eca57abeec78f7355258",
+    "url": "/lights-out/static/css/main.f4726ddf.chunk.css"
+  },
+  {
+    "revision": "fc8b4bdae2103810035d",
+    "url": "/lights-out/static/js/2.756b66bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lights-out/static/js/2.756b66bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca57abeec78f7355258",
+    "url": "/lights-out/static/js/main.15642b25.chunk.js"
+  },
+  {
+    "revision": "be864883fe966990f17a",
+    "url": "/lights-out/static/js/runtime-main.67c84857.js"
+  }
+]);
