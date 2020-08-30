b0b35e4b2ca6c764a6d87553233b9299d2d88574
ian13456 <21ianh1@students.tas.tw>
2020-08-31 01:36:23 +0800

    feat: run again

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,8 @@
 import './App.scss'
 import logo from './logo.svg'
 
-import { useTimeout } from 'react-use'
-import React, { useState } from 'react'
-import rref from 'rref'
+import React, { useEffect, useState } from 'react'
+import { Matrix, PrimeField } from 'gauss-jordan'
 
 const rules = [
   [1, 1, 0, 1, 0, 0, 0, 0, 0],
@@ -18,12 +17,34 @@ const rules = [
 ]
 
 const modulus = 3
-const colors = ['#d6e0f0', '#8d93ab', '#393b44']
+const colors = ['#393b44', '#d6e0f0', '#8d93ab']
 
 const getCellColor = (number) => colors[number % modulus]
 
+// black = 0
+// white = 1
+// gray = 2
+
 function App() {
   const [state, setState] = useState([
+    // 0,
+    // 0,
+    // 0,
+    // 0,
+    // 0,
+    // 0,
+    // 0,
+    // 0,
+    // 0
+    // 0,
+    // 0,
+    // 1,
+    // 2,
+    // 2,
+    // 1,
+    // 0,
+    // 1,
+    // 2
     Math.floor(Math.random() * modulus),
     Math.floor(Math.random() * modulus),
     Math.floor(Math.random() * modulus),
@@ -34,10 +55,126 @@ function App() {
     Math.floor(Math.random() * modulus),
     Math.floor(Math.random() * modulus)
   ])
+  const [restart, setRestart] = useState(false)
 
-  console.log(rules.map((row, i) => row.concat(state[i])))
-  const answers = rref(rules.map((row, i) => row.concat(state[i])))
-  console.log(answers)
+  const handleClick = (pairs) => {
+    setState((state) => {
+      const cloneState = JSON.parse(JSON.stringify(state))
+      pairs.forEach(([i, j]) => {
+        cloneState[i * modulus + j] = ++cloneState[i * modulus + j] % 3
+      })
+
+      return cloneState
+    })
+  }
+
+  const handleRules = (cell) => {
+    switch (cell) {
+      case '00':
+        handleClick([
+          [0, 0],
+          [0, 1],
+          [1, 0],
+          [1, 1]
+        ])
+        break
+      case '01':
+        handleClick([
+          [0, 0],
+          [0, 1],
+          [0, 2]
+        ])
+        break
+      case '02':
+        handleClick([
+          [0, 1],
+          [0, 2],
+          [1, 1],
+          [1, 2]
+        ])
+        break
+      case '10':
+        handleClick([
+          [0, 0],
+          [1, 0],
+          [2, 0]
+        ])
+        break
+      case '11':
+        handleClick([
+          [0, 1],
+          [1, 0],
+          [1, 1],
+          [1, 2],
+          [2, 1]
+        ])
+        break
+      case '12':
+        handleClick([
+          [0, 2],
+          [1, 2],
+          [2, 2]
+        ])
+        break
+      case '20':
+        handleClick([
+          [1, 0],
+          [1, 1],
+          [2, 0],
+          [2, 1]
+        ])
+        break
+      case '21':
+        handleClick([
+          [2, 0],
+          [2, 1],
+          [2, 2]
+        ])
+        break
+      case '22':
+        handleClick([
+          [1, 1],
+          [1, 2],
+          [2, 1],
+          [2, 2]
+        ])
+        break
+      default:
+        break
+    }
+  }
+
+  const augmented = rules.map((row, i) => row.concat(state[i]))
+  const matrix = new Matrix(
+    augmented.length,
+    augmented[0].length,
+    new PrimeField(modulus)
+  )
+
+  for (let i = 0; i < augmented.length; i++) {
+    for (let j = 0; j < augmented[i].length; j++)
+      matrix.set(i, j, augmented[i][j])
+  }
+
+  const answer = matrix
+    .reducedRowEchelonForm()
+    .map((row) => row[row.length - 1] * 2)
+
+  useEffect(() => {
+    const mapped = []
+    answer.forEach((n, i) => n && mapped.push(...new Array(n).fill(i)))
+
+    let counter = 0
+    const interval = setInterval(() => {
+      handleRules(`${Math.floor(mapped[counter] / 3)}${mapped[counter] % 3}`)
+      counter++
+
+      if (counter === mapped.length) {
+        setRestart(true)
+        clearInterval(interval)
+      }
+    }, 200)
+  }, [restart])
 
   return (
     <div className="App">
@@ -53,16 +190,19 @@ function App() {
             <div
               className="cell"
               id="0-0"
+              onClick={() => restart && handleRules('00')}
               style={{ backgroundColor: getCellColor(state[0]) }}
             ></div>
             <div
               className="cell"
               id="0-1"
+              onClick={() => restart && handleRules('01')}
               style={{ backgroundColor: getCellColor(state[1]) }}
             ></div>
             <div
               className="cell"
               id="0-2"
+              onClick={() => restart && handleRules('02')}
               style={{ backgroundColor: getCellColor(state[2]) }}
             ></div>
           </div>
@@ -70,16 +210,19 @@ function App() {
             <div
               className="cell"
               id="1-0"
+              onClick={() => restart && handleRules('10')}
               style={{ backgroundColor: getCellColor(state[3]) }}
             ></div>
             <div
               className="cell"
               id="1-1"
+              onClick={() => restart && handleRules('11')}
               style={{ backgroundColor: getCellColor(state[4]) }}
             ></div>
             <div
               className="cell"
               id="1-2"
+              onClick={() => restart && handleRules('12')}
               style={{ backgroundColor: getCellColor(state[5]) }}
             ></div>
           </div>
@@ -87,21 +230,32 @@ function App() {
             <div
               className="cell"
               id="2-0"
+              onClick={() => restart && handleRules('20')}
               style={{ backgroundColor: getCellColor(state[6]) }}
             ></div>
             <div
               className="cell"
               id="2-1"
+              onClick={() => restart && handleRules('21')}
               style={{ backgroundColor: getCellColor(state[7]) }}
             ></div>
             <div
               className="cell"
               id="2-2"
+              onClick={() => restart && handleRules('22')}
               style={{ backgroundColor: getCellColor(state[8]) }}
             ></div>
           </div>
         </div>
       </main>
+      <footer className="App-footer">
+        <span className="run-again" onClick={() => setRestart(false)}>
+          run again?
+        </span>
+        <br />
+        Answer:
+        <span>{answer.map((a) => a / 2)}</span>
+      </footer>
     </div>
   )
 }
